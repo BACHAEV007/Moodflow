e21b23b94c3d07f9fd9d462e93ed53a544259cdb
mvbac <mv.bachaev@gmail.com>
2025-02-25 18:15:52 +0700

    Закончил FrequentScreenFragment

--- app/src/main/java/com/example/moodflow/FrequentScreenFragment.kt
+++ app/src/main/java/com/example/moodflow/FrequentScreenFragment.kt
@@ -1,11 +1,13 @@
 package com.example.moodflow
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moodflow.adapter.FrequentEmotionAdapter
 import com.example.moodflow.databinding.FrequentEmotionsScreenBinding
+import com.example.moodflow.decorations.VerticalSpaceItemDecoration
 
 class FrequentScreenFragment : Fragment(R.layout.frequent_emotions_screen) {
     private lateinit var binding: FrequentEmotionsScreenBinding
@@ -16,7 +18,15 @@ class FrequentScreenFragment : Fragment(R.layout.frequent_emotions_screen) {
         binding = FrequentEmotionsScreenBinding.bind(view)
         binding.emotionContainer.apply {
             adapter = emotionAdapter
-            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         }
+
+        val spaceInDp = 8
+        val spaceInPx = dpToPx(spaceInDp)
+        binding.emotionContainer.addItemDecoration(VerticalSpaceItemDecoration(spaceInPx))
+    }
+
+    fun dpToPx(dp: Int): Int {
+        return (dp * Resources.getSystem().displayMetrics.density).toInt()
     }
 }
--- app/src/main/java/com/example/moodflow/adapter/FrequentEmotionAdapter.kt
+++ app/src/main/java/com/example/moodflow/adapter/FrequentEmotionAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.moodflow.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,18 +11,42 @@ import com.example.moodflow.uicontent.FrequentContent
 
 class FrequentEmotionAdapter : RecyclerView.Adapter<FrequentEmotionAdapter.FrequentViewHolder>() {
     var emotionList = mutableListOf<FrequentContent>(
-        FrequentContent(icon = R.drawable.green_image_card, emotion = "Спокойствие", count = 2),
+        FrequentContent(icon = R.drawable.green_image_card, emotion = "Спокойствие", count = 6),
         FrequentContent(icon = R.drawable.yellow_image_card, emotion = "Продуктивность", count = 1),
-        FrequentContent(icon = R.drawable.yellow_image_card, emotion = "Счастье", count = 1),
-        FrequentContent(icon = R.drawable.blue_image_card, emotion = "Выгорание", count = 1),
-        FrequentContent(icon = R.drawable.blue_image_card, emotion = "Усталость", count = 1)
+        FrequentContent(icon = R.drawable.yellow_circle_icon, emotion = "Счастье", count = 2),
+        FrequentContent(icon = R.drawable.blue_image_card, emotion = "Выгорание", count = 5),
+        FrequentContent(icon = R.drawable.blue_shell_icon, emotion = "Усталость", count = 3)
     )
     inner class FrequentViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val binding = FrequentEmotionItemBinding.bind(view)
         fun bind(item: FrequentContent) = with(binding) {
             emotionIcon.setImageResource(item.icon)
             emotionText.text = item.emotion
+            emotionCount.text = item.count.toString()
+            val maxFrequency = emotionList.maxOfOrNull { it.count }?.toFloat() ?: 1f
+            val barLength = if (maxFrequency > 0) item.count / maxFrequency else 0f
+            gradientBar.setBarParams(
+                barLength,
+                getGradientStartColor(item.icon),
+                getGradientEndColor(item.icon)
+            )
         }
+    }
+
+    private fun getGradientStartColor(iconRes: Int): Int = when (iconRes) {
+        R.drawable.green_image_card -> R.color.green_start_gradient
+        R.drawable.yellow_image_card,R.drawable.yellow_circle_icon -> R.color.yellow_start_gradient
+        R.drawable.red_image_card -> R.color.red_start_gradient
+        R.drawable.blue_shell_icon, R.drawable.blue_image_card -> R.color.blue_start_gradient
+        else -> Color.GRAY
+    }
+
+    private fun getGradientEndColor(iconRes: Int): Int = when (iconRes) {
+        R.drawable.green_image_card -> R.color.green_end_gradient
+        R.drawable.yellow_image_card,R.drawable.yellow_circle_icon -> R.color.yellow_end_gradient
+        R.drawable.red_image_card -> R.color.red_end_gradient
+        R.drawable.blue_shell_icon, R.drawable.blue_image_card -> R.color.blue_end_gradient
+        else -> Color.LTGRAY
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FrequentViewHolder {
